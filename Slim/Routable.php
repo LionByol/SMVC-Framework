cf1b49dd24139e2241cb825acb059d776cf7d9c6
LionByol <lionbyol@gmail.com>
2017-08-11 00:39:27 +0200

    Update Routable.php

--- Slim/Routable.php
+++ Slim/Routable.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Slim Framework (https://slimframework.com)
- *
- * @link      https://github.com/slimphp/Slim
- * @copyright Copyright (c) 2011-2017 Josh Lockhart
- * @license   https://github.com/slimphp/Slim/blob/3.x/LICENSE.md (MIT License)
- */
 namespace Slim;
 
 use Psr\Container\ContainerInterface;
